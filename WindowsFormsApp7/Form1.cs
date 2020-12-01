75bfd3d092c137d5431b95037c2e96e6efc84cbb
Amir Kayhani <am_kayhani@yahoo.com>
2020-12-01 10:25:07 +0000

    1.2

--- WindowsFormsApp7/Form1.cs
+++ WindowsFormsApp7/Form1.cs
@@ -16,5 +16,12 @@ namespace WindowsFormsApp7
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello");
+            //
+
+        }
     }
 }
